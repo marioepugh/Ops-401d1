dd63d763cebdd932ad6056fdd2480e17ea304ca8
marioepugh <marioepugh@gmail.com>
2020-11-18 20:49:55 -0800

    Create demo.py

---
+++ ops-401d1: Challenge 33/demo.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+
+# The below demo script works in tandem with virustotal-search.py from https://github.com/eduardxyz/virustotal-search, which must be in the same directory.
+# Set your environment variable first to keep it out of your script here.
+
+import os
+
+apikey = os.getenv('API_KEY_VIRUSTOTAL') # Set your environment variable before proceeding. You'll need a free API key from virustotal.com so get signed up there first.
+hash = 'D41D8CD98F00B204E9800998ECF8427E' # Set your hash here.
+
+# This concatenates everything into a working shell statement that gets passed into virustotal-search.py
+query = 'python3 virustotal-search.py -k ' + apikey + ' -m ' + hash
+
+os.system(query)
